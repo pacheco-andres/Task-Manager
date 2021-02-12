8b37c693b1da5171d3b9a33c0597d73eceb1e6f0
Andres Pacheco <andrespach4@gmail.com>
2021-02-12 00:05:18 -0600

    Avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   after_create :assign_default_role
+  has_one_attached :avatar
 
   def assign_default_role
     self.add_role(:student) if self.roles.blank?
